72573292f1aa3e0661a1db1f42013dcd0a7e8673
Papan Das <hexoindia@gmail.com>
2020-05-22 19:28:30 +0530

    Project Setup

--- client/client.js
+++ client/client.js
@@ -1,6 +1,3 @@
-const greet = require('../server/protos/greet_pb')
-const greetService = require('../server/protos/greet_grpc_pb');
-
 const calc = require('../server/protos/calculator_pb');
 const calcService = require('../server/protos/calculator_grpc_pb');
 
@@ -21,89 +18,6 @@ const calcProtoDefinition = protoLoader.loadSync(calcProtoPath, {
     oneofs: true
 })
 const calcPackageDefinition = grpc.loadPackageDefinition(calcProtoDefinition).greet;
-
-const services = require('../server/protos/dummy_grpc_pb')
-
-function callGreetings() {
-    var client = new greetService.GreetServiceClient(address, credentials)
-
-    var request = new greet.GreetRequest()
-    var greeting = new greet.Greeting()
-    greeting.setFirstName("Papan")
-    greeting.setLastName("Das")
-
-    request.setGreeting(greeting)
-
-    client.greet(request, (error, response) => {
-        if (!error) {
-            console.log("Greeting Resonse: ", response.getResult());
-        } else {
-            console.error(error)
-        }
-    })
-}
-
-function callDynamicGreetings() {
-    const client = new greetPackageDefinition.GreetService(address, credentials)
-
-    var request = {
-        greeting: {
-            first_name: "DynamicPapan",
-            last_name: "DynamicDas"
-        }
-    }
-
-    client.greet(request, (error, response) => {
-        if (!error) {
-            console.log("Greeting Resonse: ", response.result);
-        } else {
-            console.error(error)
-        }
-    })
-}
-
-function callGreatManyTimes() {
-    var client = new greetService.GreetServiceClient(address, credentials)
-
-    var request = new greet.GreetManyTimesRequest()
-    var greeting = new greet.Greeting()
-    greeting.setFirstName("PapanManyTimes")
-    greeting.setLastName("DasManyTimes")
-
-    request.setGreeting(greeting)
-
-    var call = client.greetManyTimes(request, (error, response) => {
-        if (!error) {
-            console.log("Greeting Resonse: ", response.getResult());
-        } else {
-            console.error(error)
-        }
-    })
-
-    call.on('status', (status) => { console.log(status); })
-
-    call.on('data', (response) => { console.log('Client Streaming Response: ', response.getResult()); })
-
-    call.on('error', (error) => { console.error(error); })
-
-    call.on('end', () => { console.log('Streaming Ended!'); })
-}
-
-function callSum() {
-    var client = new calcService.CalculatorServiceClient(address, credentials)
-
-    var sumRequest = new calc.SumRequest()
-    sumRequest.setFirstNumber(3)
-    sumRequest.setSecondNumber(10)
-
-    client.sum(sumRequest, (error, response) => {
-        if (!error) {
-            console.log("Greeting Resonse: ", response.getSumResult());
-        } else {
-            console.error(error)
-        }
-    })
-}
 
 function callPrimeNumberDecomposition() {
     var client = new calcService.CalculatorServiceClient(address, credentials)
@@ -132,13 +46,7 @@ function callPrimeNumberDecomposition() {
 function main() {
     console.log('Hello from Client')
 
-    //callGreetings()
-    //callDynamicGreetings()
-    //callGreatManyTimes()
-    //callSum();
     callPrimeNumberDecomposition()
-
-
 }
 
 main()
--- server/index.js
+++ server/index.js
@@ -1,6 +1,3 @@
-const greet = require('../server/protos/greet_pb')
-const greetService = require('../server/protos/greet_grpc_pb');
-
 const calc = require('../server/protos/calculator_pb');
 const calcService = require('../server/protos/calculator_grpc_pb');
 
@@ -12,7 +9,7 @@ const port = "127.0.0.1:50051";
 const creds = grpc.ServerCredentials.createInsecure();
 
 // Dynamic loading Proto buf
-const greetProtoPath = path.join(__dirname, "..", "protos", "greet.proto")
+const greetProtoPath = path.join(__dirname, "..", "protos", "calculator.proto")
 const greetProtoDefinition = protoLoader.loadSync(greetProtoPath, {
     keepCase: true,
     longs: String,
@@ -21,20 +18,6 @@ const greetProtoDefinition = protoLoader.loadSync(greetProtoPath, {
     oneofs: true
 })
 const greetPackageDefinition = grpc.loadPackageDefinition(greetProtoDefinition).greet;
-
-/**
- * Implementing Calculator method
- * @param {*} call 
- * @param {*} callback 
- */
-
-function sum(call, callback) {
-    var sumResponse = new calc.SumResponse();
-
-    sumResponse.setSumResult(call.request.getFirstNumber() + call.request.getSecondNumber())
-
-    callback(null, sumResponse)
-}
 
 function primeNumberDecomposition(call, callback) {
     var number = call.request.getPrimeNumber()
@@ -59,62 +42,12 @@ function primeNumberDecomposition(call, callback) {
     call.end()
 }
 
-
-/**
- * Implement GRPC greet  method
- */
-
-function greetFunc(call, callback) {
-    var greeting = new greet.GreetResponse()
-
-    console.log("Server Received: ", call.request.getGreeting().getFirstName(), call.request.getGreeting().getLastName())
-
-    greeting.setResult(
-        "Hello " + call.request.getGreeting().getFirstName()
-    )
-
-    callback(null, greeting)
-}
-
-function dynamicGreetFunc(call, callback) {
-    let firstName = call.request.greeting.first_name;
-    let lastName = call.request.greeting.last_name;
-
-    console.log("Server Received: " + firstName + " " + lastName)
-
-    callback(null, { result: "Hello " + firstName + " " + lastName })
-}
-
-
-function greetManyTimes(call, callback) {
-    let firstName = call.request.getGreeting().getFirstName()
-    let lastName = call.request.getGreeting().getLastName()
-
-
-
-    let count = 0,
-        intervalId = setInterval(function() {
-            let greetManyTimeResponse = new greet.GreetManyTimesResponse()
-            greetManyTimeResponse.setResult(firstName, lastName)
-
-            // setup streaming
-
-            call.write(greetManyTimeResponse)
-
-            if (++count > 9) {
-                clearInterval(intervalId)
-                call.end()
-            }
-        }, 1000)
-}
-
 function main() {
     const server = new grpc.Server()
 
-    // const serviceDefinition = greetService.GreetServiceService
-    server.addService(greetService.GreetServiceService, { greet: greetFunc, greetManyTimes: greetManyTimes })
-        //server.addService(greetPackageDefinition.GreetService.service, { greet: dynamicGreetFunc })
-    server.addService(calcService.CalculatorServiceService, { sum: sum, primeNumberDecomposition: primeNumberDecomposition })
+    const serviceDefinition = calcService.CalculatorServiceService
+
+    server.addService(serviceDefinition, { primeNumberDecomposition: primeNumberDecomposition })
 
     server.bind(port, creds)
     server.start()
